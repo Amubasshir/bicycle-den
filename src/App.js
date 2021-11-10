19c619e99112deea595e681a9b3b5ae622a20e86
Amubasshir <lastmubasshir@gmail.com>
2021-11-10 20:02:16 +0600

    login and register added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import
   } from "react-router-dom";
 import './App.css';
 import Home from './Pages/Home/Home/Home';
+import Login from "./Pages/Login/Login/Login";
+import Register from "./Pages/Login/Register/Register";
 
 
 function App() {
@@ -18,7 +20,13 @@ function App() {
           <Route path="/home">
             <Home />
           </Route>
-          <Route path="/">
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route exact path="/">
             <Home />
           </Route>
         </Switch>
